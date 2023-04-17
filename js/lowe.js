befa15916dd6639cacc2b0bf8b9b50f652573221
soniharsha <harsha7748@gmail.com>
2023-04-17 14:41:22 +0530

    second

---
+++ js/lowe.js
@@ -0,0 +1,11 @@
+let st='GRras';
+
+console.log(st.toLowerCase());
+st+=st[0].toLowerCase();
+console.log(st);
+
+
+console.log(Math.max(85,78,98,1000));
+console.log(Math.min(85,78,98,1000));
+
+// how to fetch mongoDb with node?  
